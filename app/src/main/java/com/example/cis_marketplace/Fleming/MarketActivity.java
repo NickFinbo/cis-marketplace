7f5aabc8a31913661286fd428887de44f374ec76
NickFinbo <nicholasjfcd2023@student.cis.edu.hk>
2022-02-26 21:50:07 +0800

    sussy_mogus

---
+++ app/src/main/java/com/example/cis_marketplace/Fleming/MarketActivity.java
@@ -0,0 +1,16 @@
+package com.example.cis_marketplace.Fleming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cis_marketplace.R;
+
+public class MarketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market);
+    }
+}
