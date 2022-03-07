fc783853b5019107442228740bde87475aae5095
lucaswu050612 <55009386+lucaswu050612@users.noreply.github.com>
2022-03-07 13:15:30 +0800

    Update UI, ItemActivity initialize

--- app/src/main/java/com/example/cis_marketplace/HomeActivity.java
+++ app/src/main/java/com/example/cis_marketplace/HomeActivity.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
-
+    EditText searchResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        searchResult = findViewById(R.id.search);
+
 
     }
     public void signOut(View v){
--- app/src/main/java/com/example/cis_marketplace/ItemActivity.java
+++ app/src/main/java/com/example/cis_marketplace/ItemActivity.java
@@ -1,17 +1,31 @@
 package com.example.cis_marketplace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ItemActivity extends AppCompatActivity {
+    TextView name;
+    TextView price;
+    TextView des;
+    TextView subject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+        name = findViewById(R.id.Name);
+        price = findViewById(R.id.Price);
+        des = findViewById(R.id.Description);
+        subject = findViewById(R.id.Subject);
     }
-    private void populateData(){
-        // get the object passed in from home activity & update UI using this information
-    }
+
 }
