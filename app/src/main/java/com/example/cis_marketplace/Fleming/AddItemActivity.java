f3c32a7094e4c8feec4669dff91065d8d1cd4846
mwong1015 <marcowong1015@gmail.com>
2022-03-13 21:32:30 +0800

    upload image function

--- app/src/main/java/com/example/cis_marketplace/Fleming/AddItemActivity.java
+++ app/src/main/java/com/example/cis_marketplace/Fleming/AddItemActivity.java
@@ -1,21 +1,36 @@
 package com.example.cis_marketplace.Fleming;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cis_marketplace.Marco.AuthActivity;
 import com.example.cis_marketplace.Nicholas.HomeActivity;
 import com.example.cis_marketplace.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 
@@ -43,6 +58,13 @@ public class AddItemActivity extends AppCompatActivity {
     FirebaseFirestore db;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
+
+    public Uri mImageUri;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private StorageTask mUploadTask;
+    private Button uploadButton;
+    Long fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,8 +100,11 @@ public class AddItemActivity extends AppCompatActivity {
         yearLevell = findViewById(R.id.yearLevelSpinner);
         type = findViewById(R.id.type);
         typee= findViewById(R.id.typeSpinner);
+        uploadButton = findViewById(R.id.uploadButton);
 
         db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, conditions);
@@ -96,7 +121,57 @@ public class AddItemActivity extends AppCompatActivity {
     }
 
     public void upload(View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,1);
+    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data!= null && data.getData()!=null) {
+            mImageUri = data.getData();
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setTitle("Uploading Image...");
+        pd.show();
+        if (mImageUri != null) {
+            StorageReference fileReference = storageRef.child("user uuid here"); // change this to user UUID later on
+            mUploadTask = fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Snackbar.make(findViewById(android.R.id.content), "Image Uploaded.", Snackbar.LENGTH_LONG).show();
+                            uploadButton.setVisibility(View.INVISIBLE);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            pd.dismiss();
+                            Toast.makeText(getApplicationContext(), "Failed To Upload", Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progressPercent = (100.00 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                            pd.setMessage("Progress " + (int) progressPercent + " %");
+                            if (progressPercent == 100.00) {
+                                pd.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
     public void homeUI(View v){
         startActivity(new Intent(this, HomeActivity.class));
